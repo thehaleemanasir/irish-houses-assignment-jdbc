1c446e3abba12ce74e56432d3cfe6491fcbb5531
Haleema Nasir <145924509+thehaleemanasir@users.noreply.github.com>
2024-08-31 16:53:38 +0100

    Bean  Handler Repo example

--- src/main/java/io/github/thehaleemanasir/repositories/PropertyBeanRepository.java
+++ src/main/java/io/github/thehaleemanasir/repositories/PropertyBeanRepository.java
@@ -4,11 +4,11 @@ import io.github.thehaleemanasir.model.Property;
 import io.github.thehaleemanasir.utils.DatabaseUtility;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 
 /**
  * Repository class using Java Beans for the Property model
@@ -29,42 +29,42 @@ public class PropertyBeanRepository {
      * @throws SQLException If an error occurs while creating the property
      */
     public int createProperty(Property property) throws SQLException {
-        String sql = "INSERT INTO properties (Street, City, ListingNum, StyleId, TypeId, Bedrooms, Bathrooms, SquareFeet, " +
+        String sql = "INSERT INTO properties (Street, City, ListingNum, StyleId, TypeId, Bedrooms, Bathrooms, SquareFeet, "
+                +
                 "BerRating, Description, LotSize, GarageSize, GarageId, AgentId, Photo, Price, DateAdded) " +
-                "VALUES (?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setString(1, property.getStreet());
-            preparedStatement.setString(2, property.getCity());
-            preparedStatement.setInt(3, property.getListingNum());
-            preparedStatement.setInt(4, property.getStyleId());
-            preparedStatement.setInt(5, property.getTypeId());
-            preparedStatement.setInt(6, property.getBedrooms());
-            preparedStatement.setFloat(7, property.getBathrooms());
-            preparedStatement.setInt(8, property.getSquareFeet());
-            preparedStatement.setString(9, property.getBerRating());
-            preparedStatement.setString(10, property.getDescription());
-            preparedStatement.setString(11, property.getLotSize());
-            preparedStatement.setByte(12, property.getGarageSize());
-            preparedStatement.setInt(13, property.getGarageId());
-            preparedStatement.setInt(14, property.getAgentId());
-            preparedStatement.setString(15, property.getPhoto());
-            preparedStatement.setDouble(16, property.getPrice());
-            preparedStatement.setDate(17, new java.sql.Date(property.getDateAdded().getTime()));
-            preparedStatement.executeUpdate();
+                "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        Object[] params = {
+                property.getStreet(),
+                property.getCity(),
+                property.getListingNum(),
+                property.getStyleId(),
+                property.getTypeId(),
+                property.getBedrooms(),
+                property.getBathrooms(),
+                property.getSquareFeet(),
+                property.getBerRating(),
+                property.getDescription(),
+                property.getLotSize(),
+                property.getGarageSize(),
+                property.getGarageId(),
+                property.getAgentId(),
+                property.getPhoto(),
+                property.getPrice(),
+                new java.sql.Date(property.getDateAdded().getTime())
+        };
 
-            int newId = DatabaseUtility.getNewId(preparedStatement);
-            property.setId(newId);
-            return newId;
-        }
+        final QueryRunner runner = new QueryRunner();
+        runner.update(connection, sql, params);
+
+        int newId = DatabaseUtility.getNewId(connection);
+        property.setId(newId);
+        return newId;
     }
 
     public boolean deleteProperty(Property property) throws SQLException {
         String sql = "DELETE FROM property WHERE Id = ?";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setInt(1, property.getId());
-            int rowsAffected = preparedStatement.executeUpdate();
-            return rowsAffected > 0;
-        }
+        final QueryRunner runner = new QueryRunner();
+        return runner.update(connection, sql, property.getId()) > 0;
     }
 
     public boolean deletePropertyById(int propertyId) throws SQLException {
@@ -77,107 +77,67 @@ public class PropertyBeanRepository {
 
     /**
      * Get all archived properties associated with a specific agent
+     * 
      * @param agentId The ID of the agent
      * @return A list of properties that are archived for the agent
      * @throws SQLException If an error occurs while fetching the properties
      */
     public List<Property> getPropertyByAgentIdAndIsArchived(int agentId) throws SQLException {
-        String query = "SELECT * FROM properties WHERE AgentId = ? AND archived = 1";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setInt(1, agentId);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                List<Property> properties = new ArrayList<>();
-                while (resultSet.next()) {
-                    properties.add(mapResultSetToProperty(resultSet));
-                }
-                return properties;
-            }
-        }
+        String sql = "SELECT * FROM properties WHERE AgentId = ? AND archived = 1";
+        final QueryRunner runner = new QueryRunner();
+        final var listHandler = new BeanListHandler<>(Property.class);
+        return runner.query(connection, sql, listHandler, agentId);
     }
 
     public Property getPropertyById(int propertyId) throws SQLException {
-        String query = "SELECT * FROM properties WHERE Id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setInt(1, propertyId);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    return mapResultSetToProperty(resultSet);
-                }
-                return null;
-            }
-        }
-    }
+        String sql = "SELECT * FROM properties WHERE Id = ?";
 
-
-    private Property mapResultSetToProperty(ResultSet resultSet) throws SQLException {
-        Property property = new Property();
-        property.setId(resultSet.getInt("Id"));
-        property.setStreet(resultSet.getString("Street"));
-        property.setCity(resultSet.getString("City"));
-        property.setListingNum(resultSet.getInt("ListingNum"));
-        property.setStyleId(resultSet.getInt("StyleId"));
-        property.setTypeId(resultSet.getInt("TypeId"));
-        property.setBedrooms(resultSet.getInt("Bedrooms"));
-        property.setBathrooms(resultSet.getFloat("Bathrooms"));
-        property.setSquareFeet(resultSet.getInt("SquareFeet"));
-        property.setBerRating(resultSet.getString("BerRating"));
-        property.setDescription(resultSet.getString("Description"));
-        property.setLotSize(resultSet.getString("LotSize"));
-        property.setGarageSize(resultSet.getByte("GarageSize"));
-        property.setGarageId(resultSet.getInt("GarageId"));
-        property.setAgentId(resultSet.getInt("AgentId"));
-        property.setPhoto(resultSet.getString("Photo"));
-        property.setPrice(resultSet.getDouble("Price"));
-        property.setDateAdded(new java.util.Date(resultSet.getDate("DateAdded").getTime()));
-        return property;
+        final QueryRunner runner = new QueryRunner();
+        final var handler = new BeanHandler<>(Property.class);
+        return runner.query(connection, sql, handler, propertyId);
     }
 
     public List<Property> getAllProperties() throws SQLException {
-        String query = "SELECT * FROM properties";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            try (ResultSet resultSet = statement.executeQuery()) {
-                List<Property> properties = new ArrayList<>();
-                while (resultSet.next()) {
-                    properties.add(mapResultSetToProperty(resultSet));
-                }
-                return properties;
-            }
-        }
+        String sql = "SELECT * FROM properties";
+        final QueryRunner runner = new QueryRunner();
+        final var listHandler = new BeanListHandler<>(Property.class);
+        return runner.query(connection, sql, listHandler);
     }
 
-    public void archiveProperty(Property property) throws SQLException {
+    public boolean archiveProperty(Property property) throws SQLException {
         String sql = "UPDATE properties SET archived = 1 WHERE id = ?;";
-        try (PreparedStatement archiveStatement = connection.prepareStatement(sql)) {
-            archiveStatement.setInt(1, property.getId());
-            archiveStatement.executeUpdate();
-        }
+        final QueryRunner runner = new QueryRunner();
+        return runner.update(connection, sql, property.getId()) > 0;
     }
 
     public void updateProperty(Property property) throws SQLException {
-        String sql = "UPDATE properties SET Street = ?, City = ?, ListingNum = ?, StyleId = ?, TypeId = ?, Bedrooms = ?, " +
-                "Bathrooms = ?, SquareFeet = ?, BerRating = ?, Description = ?, LotSize = ?, GarageSize = ?, GarageId = ?, " +
+        String sql = "UPDATE properties SET Street = ?, City = ?, ListingNum = ?, StyleId = ?, TypeId = ?, Bedrooms = ?, "
+                +
+                "Bathrooms = ?, SquareFeet = ?, BerRating = ?, Description = ?, LotSize = ?, GarageSize = ?, GarageId = ?, "
+                +
                 "AgentId = ?, Photo = ?, Price = ?, DateAdded = ? WHERE Id = ?";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setString(1, property.getStreet());
-            preparedStatement.setString(2, property.getCity());
-            preparedStatement.setInt(3, property.getListingNum());
-            preparedStatement.setInt(4, property.getStyleId());
-            preparedStatement.setInt(5, property.getTypeId());
-            preparedStatement.setInt(6, property.getBedrooms());
-            preparedStatement.setFloat(7, property.getBathrooms());
-            preparedStatement.setInt(8, property.getSquareFeet());
-            preparedStatement.setString(9, property.getBerRating());
-            preparedStatement.setString(10, property.getDescription());
-            preparedStatement.setString(11, property.getLotSize());
-            preparedStatement.setByte(12, property.getGarageSize());
-            preparedStatement.setInt(13, property.getGarageId());
-            preparedStatement.setInt(14, property.getAgentId());
-            preparedStatement.setString(15, property.getPhoto());
-            preparedStatement.setDouble(16, property.getPrice());
-            preparedStatement.setDate(17, new java.sql.Date(property.getDateAdded().getTime()));
-            preparedStatement.setInt(18, property.getId());
-            preparedStatement.executeUpdate();
-        }
+        Object[] params = {
+                property.getStreet(),
+                property.getCity(),
+                property.getListingNum(),
+                property.getStyleId(),
+                property.getTypeId(),
+                property.getBedrooms(),
+                property.getBathrooms(),
+                property.getSquareFeet(),
+                property.getBerRating(),
+                property.getDescription(),
+                property.getLotSize(),
+                property.getGarageSize(),
+                property.getGarageId(),
+                property.getAgentId(),
+                property.getPhoto(),
+                property.getPrice(),
+                new java.sql.Date(property.getDateAdded().getTime()),
+                property.getId()
+        };
 
+        final QueryRunner runner = new QueryRunner();
+        runner.update(connection, sql, params);
     }
 }
