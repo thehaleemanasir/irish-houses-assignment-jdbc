9448723fc831e7fb2eb86e9673a539a3565306f1
Haleema Nasir <145924509+thehaleemanasir@users.noreply.github.com>
2024-08-31 16:51:29 +0100

    dependency and util method

--- src/main/java/io/github/thehaleemanasir/utils/DatabaseUtility.java
+++ src/main/java/io/github/thehaleemanasir/utils/DatabaseUtility.java
@@ -12,12 +12,52 @@ public class DatabaseUtility {
     }
 
     /**
+     * Retrieves the new ID for an inserted record from the database connection.
+     * This method is used to get the new Auto-Incremented Column value of the newly
+     * inserted record.
+     * It assumes that the ID column is the first column in the ResultSet.
+     * 
+     * @param connection the database connection used to execute the insert
+     *                   statement
+     * @return the generated key as an Integer
+     * @throws SQLException if no key is obtained or a database access error occurs
+     * 
+     *                      <p>
+     *                      Example:
+     * 
+     *                      <pre>
+     *                      String query = "INSERT INTO table_name (column_name) VALUES (?)";
+     *                      try (Connection connection = DatabaseUtility.getConnection()) {
+     *                          PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+     *                          preparedStatement.setString(1, "value");
+     *                          preparedStatement.executeUpdate();
+     *                          int newId = DatabaseUtility.getNewId(connection);
+     *                          System.out.println("New ID: " + newId);
+     *                      }
+     *                      </pre>
+     *                      </p>
+     */
+    public static Integer getNewId(Connection connection) throws SQLException {
+        String sql = "SELECT LAST_INSERT_ID()";
+        try (Statement stmt = connection.createStatement();
+                ResultSet rs = stmt.executeQuery(sql)) {
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        }
+        throw new SQLException("Creating property failed, no ID obtained.");
+    }
+
+    /**
      * Get the new ID for an inserted record from the prepared statement.
-     * Basically, this method is used to get the new Auto-Incremented Column value of the newly inserted record.
-     * Your PreparedStatement must be created with the Statement.RETURN_GENERATED_KEYS flag.
+     * Basically, this method is used to get the new Auto-Incremented Column value
+     * of the newly inserted record.
+     * Your PreparedStatement must be created with the
+     * Statement.RETURN_GENERATED_KEYS flag.
      * Assuming the ID column is the first column in the ResultSet.
      * <p>
      * Example:
+     * 
      * <pre>
      *         String query = "INSERT INTO table_name (column_name) VALUES (?)";
      *         try (Connection connection = DatabaseUtility.getConnection();) {
@@ -27,7 +67,7 @@ public class DatabaseUtility {
      *             int newId = DatabaseUtility.getNewId(preparedStatement);
      *             System.out.println("New ID: " + newId);
      *         }
-     *         </pre>
+     * </pre>
      * </p>
      *
      * @param preparedStatement The PreparedStatement used to insert the record.
@@ -40,10 +80,13 @@ public class DatabaseUtility {
 
     /**
      * Get the new ID for an inserted record from the prepared statement.
-     * Basically, this method is used to get the new Auto-Incremented Column value of the newly inserted record.
-     * Your PreparedStatement must be created with the Statement.RETURN_GENERATED_KEYS flag.
+     * Basically, this method is used to get the new Auto-Incremented Column value
+     * of the newly inserted record.
+     * Your PreparedStatement must be created with the
+     * Statement.RETURN_GENERATED_KEYS flag.
      * <p>
      * Example:
+     * 
      * <pre>
      *         String query = "INSERT INTO table_name (column_name) VALUES (?)";
      *         try (Connection connection = DatabaseUtility.getConnection();) {
@@ -53,7 +96,7 @@ public class DatabaseUtility {
      *             int newId = DatabaseUtility.getNewId(preparedStatement);
      *             System.out.println("New ID: " + newId);
      *         }
-     *         </pre>
+     * </pre>
      * </p>
      *
      * @param preparedStatement The PreparedStatement used to insert the record.
@@ -69,7 +112,6 @@ public class DatabaseUtility {
         }
         throw new SQLException("Creating property failed, no ID obtained.");
     }
-
 
     private DatabaseUtility() {
         throw new IllegalStateException("Utility class");
